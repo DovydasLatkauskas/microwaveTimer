3c661d62844e844a1913b219b8e50149ee7557f3
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-13 15:07:28 +0000

    added functionality

--- Main.java
+++ Main.java
@@ -7,7 +7,34 @@ public class Main {
         System.out.println(microwaveTimer(testInput));
     }
 
-    public static ArrayList<ArrayList<Integer>> microwaveTimer(ArrayList<Integer> input) {
+    private static ArrayList<ArrayList<Integer>> microwaveTimer(ArrayList<Integer> input) {
+        ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
+        output.add(input); // adds the input parameters to the beginning of the output
+        while (!((input.get(0) == 0)&&(input.get(1) == 0))) {
+            output.add(countDown(input));
+        }
+        // adds [0,0] to the end off output
+        ArrayList<Integer> endList = new ArrayList<Integer>();
+        endList.add(0);
+        endList.add(0);
+        output.add(endList);
+        return output;
+    }
 
+    private static ArrayList<Integer> countDown(ArrayList<Integer> input) {
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        if (input.get(1) > 0) {
+            output.add(input.get(0));
+            output.add(input.get(1) - 1);
+            return output;
+        }
+        else if((input.get(1) == 0) && (input.get(0) > 0)){
+            output.add(input.get(0) - 1);
+            output.add(59);
+            return output;
+        }
+        else{
+            throw new IllegalStateException("countDown method logic error");
+        }
     }
 }
