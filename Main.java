dc3798a8489adf2596e988214cd1ff03cfd88bb1
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-13 14:41:09 +0000

    initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> testInput = new ArrayList<Integer>();
+        testInput.add(1);
+        testInput.add(80);
+        System.out.println(microwaveTimer(testInput));
+    }
+
+    public static ArrayList<ArrayList<Integer>> microwaveTimer(ArrayList<Integer> input) {
+
+    }
+}
