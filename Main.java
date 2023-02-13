557cecd89f46b03f2209fae09f5650d68b2a5ad8
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-13 15:14:12 +0000

    added JavaDoc comments

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> testInput = new ArrayList<Integer>();
+        ArrayList<Integer> testInput = new ArrayList<Integer>(); //input created for testing
         testInput.add(1);
         testInput.add(80);
-        System.out.println(microwaveTimer(testInput));
+        System.out.println(microwaveTimer(testInput)); // print statement for testing
     }
+
+    /**
+     * Main function of the program
+     * represents the working of the microwave timer
+     * 
+     * @param input the starting state of the microwave timer
+     * @return an ArrayList of integers integers representing the state of
+     * the microwave timer ordered chronologically
+     */
 
     private static ArrayList<ArrayList<Integer>> microwaveTimer(ArrayList<Integer> input) {
         ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
@@ -13,13 +22,19 @@ public class Main {
         while (!((input.get(0) == 0)&&(input.get(1) == 0))) {
             output.add(countDown(input));
         }
-        // adds [0,0] to the end off output
+        // adds [0,0] to the end of output
         ArrayList<Integer> endList = new ArrayList<Integer>();
         endList.add(0);
         endList.add(0);
         output.add(endList);
         return output;
     }
+
+    /**
+     * helper function for microwaveTimer()
+     * @param input the current state of the timer
+     * @return the state of the timer after a second
+     */
 
     private static ArrayList<Integer> countDown(ArrayList<Integer> input) {
         ArrayList<Integer> output = new ArrayList<Integer>();
@@ -33,7 +48,7 @@ public class Main {
             output.add(59);
             return output;
         }
-        else{
+        else{ // should never fire
             throw new IllegalStateException("countDown method logic error");
         }
     }
